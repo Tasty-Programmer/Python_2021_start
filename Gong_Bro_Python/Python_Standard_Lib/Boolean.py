4b78b18a594d979d685305267fc29c8df860d8b9
komegKim <kimhunsup159@gmail.com>
2021-10-10 00:57:10 +0900

    Boolean.py

---
+++ Gong_Bro_Python/Python_Standard_Lib/Boolean.py
@@ -0,0 +1,18 @@
+# 흐름 제어
+
+print(True and True)
+print(True and False)
+print(True and False and False)
+print(True or True)
+print(True or False)
+print(True or False or False)
+
+print(not True)
+print(not False)
+print(100 and 1)
+print(100 and 0)
+print(100 or 1)
+print(100 or 0)
+print(1 and 0 and 100)
+print(1 and 10 and 100 and 1000)
+print()
